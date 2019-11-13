6f75c06cc1934db7b620f2c28fb605d9d74ff998
Rajendra Singh Rana <40059374+raj1rana@users.noreply.github.com>
2019-11-13 17:56:14 +0530

    Update fields.py

--- optimized_image/fields.py
+++ optimized_image/fields.py
@@ -5,7 +5,7 @@ class OptimizedImageField(ImageField):
     """An ImageField that gets optimized on save() using tinyPNG."""
     def save_form_data(self, instance, data):
         """Remove the OptimizedNotOptimized object on clearing the image."""
-        # Are we updating an image?
+        # Are we updatiwdwdng an image?
         updating_image = True if data and getattr(instance, self.name) != data else False
 
         if updating_image:
